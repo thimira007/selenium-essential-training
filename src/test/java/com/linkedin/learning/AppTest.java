515a733d5902d72ba69dcd5a11b9374fb5ebc807
thimira007 <thimira007@gmail.com>
2020-04-12 08:23:45 +0530

    updated tests

--- src/test/java/com/linkedin/learning/AppTest.java
+++ src/test/java/com/linkedin/learning/AppTest.java
@@ -3,14 +3,14 @@ package com.linkedin.learning;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.CheckedInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -43,6 +43,7 @@ public class AppTest {
     public void autoCompleteTest() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5,  TimeUnit.SECONDS);
 
         driver.get("https://formy-project.herokuapp.com/autocomplete");
         WebElement address = driver.findElement(By.id("autocomplete"));
@@ -136,9 +137,97 @@ public class AppTest {
         WebElement drop = driver.findElement(By.id("box"));
 
         Actions actions = new Actions(driver);
-        actions.dragAndDrop(image,drop).build().perform();
+        actions.dragAndDrop(image, drop).build().perform();
         waitFor(5);
         driver.quit();
+    }
+
+    @Test
+    public void radioButtons() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/radiobutton");
+
+        WebElement radio1 = driver.findElement(By.id("radio-button-1"));
+        WebElement radio2 = driver.findElement(By.xpath("//div[@class='form-check']//input[@value='option2']"));
+        WebElement radio3 = driver.findElement(By.xpath("//div[@class='form-check']//input[@value='option3']"));
+        radio1.click();
+        waitFor(2);
+        radio2.click();
+        waitFor(2);
+        radio3.click();
+        waitFor(2);
+        driver.quit();
+    }
+
+    @Test
+    public void selectCheckbox() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/checkbox");
+
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@id='checkbox-1']"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//input[@id='checkbox-2']"));
+        WebElement checkbox3 = driver.findElement(By.xpath("//input[@id='checkbox-3']"));
+
+        selectCheckbox(checkbox1, true);
+        waitFor(2);
+
+        selectCheckbox(checkbox3, true);
+        waitFor(2);
+
+        selectCheckbox(checkbox2, false);
+        waitFor(2);
+
+        selectCheckbox(checkbox1, false);
+        waitFor(2);
+
+        selectCheckbox(checkbox3, true);
+        waitFor(2);
+
+        driver.quit();
+    }
+
+    @Test
+    public void datePicker() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/datepicker");
+        WebElement dateField = driver.findElement(By.xpath("//input[@id='datepicker']"));
+        dateField.sendKeys("03/02/2020");
+        dateField.sendKeys(Keys.ENTER);
+        waitFor(2);
+        driver.quit();
+    }
+
+    @Test
+    public void dropDown() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/dropdown");
+        selectFromDropDown(driver,"Checkbox");
+        selectFromDropDown(driver,"Modal");
+        driver.quit();
+    }
+
+    private void selectFromDropDown(WebDriver driver, String selectOption) {
+        WebElement dropdown = driver.findElement(By.id("dropdownMenuButton"));
+        dropdown.click();
+        waitFor(1);
+        String xpath = "//div[@class='dropdown-menu show']/a[text()='" + selectOption + "']";
+        WebElement option = driver.findElement(By.xpath(xpath));
+        waitFor(1);
+        option.click();
+        waitFor(2);
+        driver.navigate().back();
+    }
+
+    private void selectCheckbox(WebElement checkbox, boolean isChecked) {
+        if (checkbox.isSelected() == isChecked) {
+            System.out.println("Checkbox is already in the given state(isChecked: )" + isChecked + ". " + checkbox);
+        } else {
+            checkbox.click();
+        }
     }
 
     private void waitFor(int seconds) {
